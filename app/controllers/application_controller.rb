539c0cb034cc1c9107cbdd352c13592807dfa9ef
rest515 <rest515@gmail.com>
2016-04-06 22:34:28 +0900

    publishing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :detect_browser
+
+  private
+
+  def detect_browser
+    request.variant = :mobile if browser.device.mobile?
+  end
 end
